1c90b1e961273c6fb17fcfd4134cb3c90be1056d
Phuc Nguyen <52201301+phucnx00@users.noreply.github.com>
2020-09-21 00:49:59 +0700

    Add files via upload

---
+++ Bai9.java
@@ -0,0 +1,15 @@
+package Buoi_3;
+
+public class Bai9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Welcome";
+		String s2 = "weacome";
+		boolean x;
+		System.out.println( x = s1.equals(s2));
+		
+		System.out.println(s1.compareToIgnoreCase(s2));
+	}
+
+}
